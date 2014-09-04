b066220e62bf813f3652ab7205fbf9b3092a926f
luetkemj <luetkemj@gmail.com>
2014-09-04 11:05:36 -0400

    fixed some compilation errors in scss and put the get_developments_scripts() function in footer

--- footer.php
+++ footer.php
@@ -29,6 +29,8 @@
 		<?php // all js scripts are loaded in library/bones.php ?>
 		<?php wp_footer(); ?>
 
+    <?php echo get_development_scripts(); ?>
+
 	</body>
 
 </html> <!-- end of site. what a ride! -->
