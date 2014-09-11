01f7f8647c29fdbffc0e3179d3a40f53bd86d62c
Cody W <cody@codywhitby.net>
2014-09-11 14:47:36 -0400

    adding vcubar

--- header.php
+++ header.php
@@ -40,6 +40,15 @@
 	</head>
 
 	<body <?php body_class(); ?>>
+  
+  <div class="vcubar">
+    <div class="inner-vcubar">
+      <a href="http://vcu.edu" target="_blank" class="vcu"><?php include (TEMPLATEPATH . '/library/images/svg/logo-vcu.svg'); ?></a>
+      <a href="http://vcu.edu" target="_blank" class="vcu-fallback"></a>
+      <a href="http://arts.vcu.edu" target="_blank" class="vcuarts"><?php include (TEMPLATEPATH . '/library/images/svg/logo-vcuarts.svg'); ?></a>
+      <a href="http://arts.vcu.edu" target="_blank" class="vcuarts-fallback"></a>
+    </div>
+  </div>
 
 		<div id="container">
 
