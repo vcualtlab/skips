71737fedb1d2fa8deeda8f33897dddc38f283541
Cody W <cody@codywhitby.net>
2014-09-16 11:09:07 -0400

    fixing vcubar bugs in safari

--- functions.php
+++ functions.php
@@ -237,9 +237,9 @@ function get_development_scripts(){
     return "
     <script src='//localhost:35729/livereload.js'></script> 
 
-    <script src='//172.23.54.26:3000/socket.io/socket.io.js'></script>
-    <script>var ___socket___ = io.connect('http://172.23.54.26:3000');</script>
-    <script src='//172.23.54.26:3001/client/browser-sync-client.0.8.2.js'></script>";
+    <script src='//172.23.48.217:3000/socket.io/socket.io.js'></script>
+    <script>var ___socket___ = io.connect('http://172.23.48.217:3000');</script>
+    <script src='//172.23.48.217:3001/client/browser-sync-client.0.8.2.js'></script>";
   }
 
 }
