9e7ed1dbfa6e69fd9a4ae98d956deda9836261b2
luetkemj <luetkemj@gmail.com>
2015-02-05 15:28:44 -0500

    page loop for docs

--- page.php
+++ page.php
@@ -6,7 +6,45 @@
 
 					<main id="main" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+<?php
+/**
+* WordPress Query Comprehensive Reference
+* Compiled by luetkemj - luetkemj.com
+*
+* CODEX: http://codex.wordpress.org/Class_Reference/WP_Query#Parameters
+* Source: https://core.trac.wordpress.org/browser/tags/3.9/src/wp-includes/query.php
+*/
+ 
+// figure out what page we are on. 
+// parent or child
+// if we are on a child page redirect to parent
+// if we are on parent show that page
+// show all children below and only children of parent lecture
+
+
+							if ($post->post_parent)	{
+								$ancestors=get_post_ancestors($post->ID);
+								$root=count($ancestors)-1;
+								$parent = $ancestors[$root];
+							} else {
+								$parent = $post->ID;
+							}
+
+
+
+							$args = array( 
+								'posts_per_page' => 1,
+								'post_type' => 'page',
+								'page_id' => $parent,
+								// 'post_parent' => $parent
+							);
+
+							$the_query = new WP_Query( $args );
+
+							// The Loop
+							if ( $the_query->have_posts() ) :
+							while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 
 							<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 
@@ -14,43 +52,17 @@
 
 									<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
 
-									<p class="byline vcard">
-										<?php printf( __( 'Posted <time class="updated" datetime="%1$s" itemprop="datePublished">%2$s</time> by <span class="author">%3$s</span>', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), get_the_author_link( get_the_author_meta( 'ID' ) )); ?>
-									</p>
-
 								</header> <?php // end article header ?>
 
 								<section class="entry-content" itemprop="articleBody">
 									<?php
-										// the content (pretty self explanatory huh)
 										the_content();
-
-										/*
-										 * Link Pages is used in case you have posts that are set to break into
-										 * multiple pages. You can remove this if you don't plan on doing that.
-										 *
-										 * Also, breaking content up into multiple pages is a horrible experience,
-										 * so don't do it. While there are SOME edge cases where this is useful, it's
-										 * mostly used for people to get more ad views. It's up to you but if you want
-										 * to do it, you're wrong and I hate you. (Ok, I still love you but just not as much)
-										 *
-										 * http://gizmodo.com/5841121/google-wants-to-help-you-avoid-stupid-annoying-multiple-page-articles
-										 *
-										*/
-										wp_link_pages( array(
-											'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'bonestheme' ) . '</span>',
-											'after'       => '</div>',
-											'link_before' => '<span>',
-											'link_after'  => '</span>',
-										) );
 									?>
 								</section> <?php // end article section ?>
 
 								<footer class="article-footer">
 
 								</footer>
-
-								<?php comments_template(); ?>
 
 							</article>
 
@@ -70,9 +82,102 @@
 
 							<?php endif; ?>
 
-						</main>
 
-						<?php get_sidebar(); ?>
+<?php 
+
+// Reset Post Data
+wp_reset_postdata();
+
+?>
+
+
+
+
+
+
+<?php
+/**
+* WordPress Query Comprehensive Reference
+* Compiled by luetkemj - luetkemj.com
+*
+* CODEX: http://codex.wordpress.org/Class_Reference/WP_Query#Parameters
+* Source: https://core.trac.wordpress.org/browser/tags/3.9/src/wp-includes/query.php
+*/
+ 
+// figure out what page we are on. 
+// parent or child
+// if we are on a child page redirect to parent
+// if we are on parent show that page
+// show all children below and only children of parent lecture
+
+
+$children = wp_list_pages('title_li=&echo=0&child_of=' . $parent);
+if ($children){
+							$args = array( 
+								'posts_per_page' => -1,
+								'post_type' => 'page',
+								// 'page_id' => $parent,
+								'post_parent' => $parent
+							);
+
+							$the_query = new WP_Query( $args );
+
+							// The Loop
+							if ( $the_query->have_posts() ) :
+							while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+
+							<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+
+								<header class="article-header">
+
+									<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
+
+								</header> <?php // end article header ?>
+
+								<section class="entry-content" itemprop="articleBody">
+									<?php
+										the_content();
+									?>
+								</section> <?php // end article section ?>
+
+								<footer class="article-footer">
+
+								</footer>
+
+							</article>
+
+							<?php endwhile; else : ?>
+
+									<article id="post-not-found" class="hentry">
+										<header class="article-header">
+											<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+										</header>
+										<section class="entry-content">
+											<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+										</section>
+										<footer class="article-footer">
+												<p><?php _e( 'This is the error message in the page.php template.', 'bonestheme' ); ?></p>
+										</footer>
+									</article>
+
+							<?php endif; ?>
+
+
+<?php 
+
+// Reset Post Data
+wp_reset_postdata();
+
+
+}
+
+?>
+							
+
+
+
+
+						</main>
 
 				</div>
 
