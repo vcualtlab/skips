c6f93d6dc052b5a15168127510355934bb28964d
luetkemj <luetkemj@gmail.com>
2015-02-05 13:48:45 -0500

    base layout and some tweaky bits

--- index.php
+++ index.php
@@ -14,12 +14,12 @@
 
 									<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 									<p class="byline entry-meta vcard">
-                                        				<?php printf( __( 'Posted %1$s by %2$s', 'bonestheme' ),
-                       								/* the time the post was published */
-                       								'<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
-                       								/* the author of the post */
-                       								'<span class="by">by</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
-                    							); ?>
+                    <?php printf( __( 'Posted %1$s by %2$s', 'bonestheme' ),
+                    		/* the time the post was published */
+                    		'<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
+                    		/* the author of the post */
+                    		'<span class="by">by</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
+                    ); ?>
 									</p>
 
 								</header>
@@ -65,8 +65,6 @@
 
 
 						</main>
-
-					<?php get_sidebar(); ?>
 
 				</div>
 
