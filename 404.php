6818c8d4bd7acfc733a4d37236fa326b1761e8d5
Dan <dstrogiy@gmail.com>
2015-02-06 15:25:44 -0500

    404 page init

--- 404.php
+++ 404.php
@@ -8,29 +8,20 @@
 
 						<article id="post-not-found" class="hentry">
 
-							<header class="article-header">
 
-								<h1><?php _e( 'Epic 404 - Article Not Found', 'bonestheme' ); ?></h1>
 
-							</header>
+								<h1><?php _e( '404 Error', 'bonestheme' ); ?></h1>
 
-							<section class="entry-content">
+
+
+
+
+								<h3>Nothing was found.</h3>
 
 								<p><?php _e( 'The article you were looking for was not found, but maybe try looking again!', 'bonestheme' ); ?></p>
 
-							</section>
 
-							<section class="search">
 
-									<p><?php get_search_form(); ?></p>
-
-							</section>
-
-							<footer class="article-footer">
-
-									<p><?php _e( 'This is the 404.php template.', 'bonestheme' ); ?></p>
-
-							</footer>
 
 						</article>
 
