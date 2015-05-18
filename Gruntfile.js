59fbab852c675fb6291b14292a8e707cb96afb27
Mark Luetke <luetkemj@Enochs-MacBook-Pro.local>
2015-05-18 10:20:48 -0400

    update Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -127,17 +127,7 @@ module.exports = function(grunt) {
         copy: {
 		  main: {
 		    files: [
-		      // includes files within path
-		      // {expand: true, src: ['path/*'], dest: 'dest/', filter: 'isFile'},
-
-		      // includes files within path and its sub-directories
-		      {expand: true, src: ['**','!bower_components/**','!node_modules/**','!.git/**'], dest: 'build/'},
-
-		      // makes all src relative to cwd
-		      // {expand: true, cwd: 'path/', src: ['**'], dest: 'dest/'},
-
-		      // flattens results to a single level
-		      // {expand: true, flatten: true, src: ['path/**'], dest: 'dest/', filter: 'isFile'},
+		      {expand: true, src: ['**','!build/**','!bower_components/**','!node_modules/**','!.git/**'], dest: 'build/'},
 		    ],
 		  },
 		},
